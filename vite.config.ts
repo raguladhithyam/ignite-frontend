4511d93bf9de1e69e5f8ba9ddc03899da3556b7b
raguladhithyam <mra20031006@gmail.com>
2025-06-19 12:52:40 +0530

    updates in url

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ignite-backend-ak0h.onrender.com/',
         changeOrigin: true,
       },
     },
