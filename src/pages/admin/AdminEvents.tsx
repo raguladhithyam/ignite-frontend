bd4247cb285828ddca0f8e62260071949204a717
Ragul Adhithya M <mra20031006@gmail.com>
2025-06-27 15:47:51 +0530

    Update AdminEvents.tsx

--- src/pages/admin/AdminEvents.tsx
+++ src/pages/admin/AdminEvents.tsx
@@ -69,8 +69,8 @@ export default function AdminEvents() {
         anEnabled: true,
         fnStartTime: '08:45',
         fnEndTime: '09:15',
-        anStartTime: '14:00',
-        anEndTime: '17:30'
+        anStartTime: '15:30',
+        anEndTime: '16:00'
       })
     }
 
