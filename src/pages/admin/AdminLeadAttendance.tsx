4ef699801578db4805c4a9e2f6c67ad7a7439e37
raguladhithyam <mra20031006@gmail.com>
2025-06-29 16:18:56 +0530

    admin lead attendance updated

--- src/pages/admin/AdminLeadAttendance.tsx
+++ src/pages/admin/AdminLeadAttendance.tsx
@@ -1,19 +1,18 @@
 export default function AdminLeadAttendance() {
   return (
-    <div className="fixed inset-0 -m-6 lg:-m-8">
-      <iframe
-        src="https://leads-attendance.vercel.app"
-        className="w-full h-full border-0"
-        title="Lead Attendance Management"
-        allow="fullscreen"
-        style={{ 
-          width: '100vw', 
-          height: '100vh',
-          position: 'absolute',
-          top: 0,
-          left: 0
-        }}
-      />
+    <div className="w-full h-full">
+      {/* Container that respects the layout structure */}
+      <div className="relative w-full" style={{ 
+        height: 'calc(100vh - var(--header-height, 64px))',
+        marginTop: 'var(--header-height, 64px)'
+      }}>
+        <iframe 
+          src="https://leads-attendance.vercel.app" 
+          className="w-full h-full border-0 block"
+          title="Lead Attendance Management" 
+          allow="fullscreen"
+        />
+      </div>
     </div>
-  )
+  );
 }
