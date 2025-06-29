43a1e55c26be63534a9dc3ef4c83b0814516fb4e
raguladhithyam <mra20031006@gmail.com>
2025-06-29 16:22:42 +0530

    page css update

--- src/pages/admin/AdminLeadAttendance.tsx
+++ src/pages/admin/AdminLeadAttendance.tsx
@@ -1,11 +1,8 @@
 export default function AdminLeadAttendance() {
   return (
     <div className="w-full h-full">
-      {/* Container that respects the layout structure */}
-      <div className="relative w-full" style={{ 
-        height: 'calc(100vh - var(--header-height, 64px))',
-        marginTop: 'var(--header-height, 64px)'
-      }}>
+      {/* Container that starts right after header */}
+      <div className="relative w-full h-screen">
         <iframe 
           src="https://leads-attendance.vercel.app" 
           className="w-full h-full border-0 block"
