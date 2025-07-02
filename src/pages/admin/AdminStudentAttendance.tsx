602db95ff54f776cdb08a61f72bedcf957ed3bc6
raguladhithyam <mra20031006@gmail.com>
2025-07-02 17:09:53 +0530

    dual filter removed

--- src/pages/admin/AdminStudentAttendance.tsx
+++ src/pages/admin/AdminStudentAttendance.tsx
@@ -485,39 +485,6 @@ export default function AdminStudentAttendance() {
         </CardContent>
       </Card>
 
-      {/* Filters */}
-      <Card>
-        <CardContent className="p-6">
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex-1">
-              <div className="relative">
-                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
-                <Input
-                  placeholder="Search students..."
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  className="pl-10"
-                />
-              </div>
-            </div>
-            <div className="w-full md:w-48">
-              <select
-                value={selectedBrigade}
-                onChange={(e) => setSelectedBrigade(e.target.value)}
-                className="w-full h-10 px-3 rounded-md border border-input bg-background text-sm"
-              >
-                <option value="">All Brigades</option>
-                {brigades.map((brigade) => (
-                  <option key={brigade.id} value={brigade.id}>
-                    {brigade.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-
       {/* Bulk Actions */}
       {selectedStudents.size > 0 && (
         <Card>
